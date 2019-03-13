d74ac6447108b7ab9286632825c34013cb7c1c28
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-03-12 17:02:32 -0700

    refactored and added distinct colors

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
 $(function() {
-
+  
   'use strict';
 
+  // jQuery object of all container class elements
   let $containers = $(".container");
 
+  // returns a random percentage as a string with '%' at end
+  const randPercent = () => Math.floor(Math.random() * 100) + 1 + '%';
+
+  // changes color of container elements based on screen size change
   $(window).on('resize', function() {
+    let width = $(window).width();
+    // for each container element
     $containers.each(function() {
-      $(this).css('background-color', `#${Math.ceil((Math.random() + 1) * 100000)}`);
+      let colorOpacity;
+
+      if (width <= 768) {
+        // mobile view
+        colorOpacity = 1.0;
+      } else if (width <= 960) {
+        // tablet view
+        colorOpacity = 0.35;
+      } else {
+        // desktop view
+        colorOpacity = 0.05;
+      }
+
+      // $(this) instead of this, so we can use jQuery methods on it
+      $(this).css('background-color', `rgb(${randPercent()}, ${randPercent()}, 
+                                           ${randPercent()}, ${colorOpacity})`);
     });
   });
-
 });
